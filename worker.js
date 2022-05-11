7631111f859a78d2ddbb9dab03fc02804beaa8e5
Jonathan Edey <55252373+jkyle109@users.noreply.github.com>
2022-05-11 04:44:51 -0400

    package commands

---
+++ worker.js
@@ -0,0 +1,33 @@
+import concurrently from "concurrently";
+
+concurrently(
+  [
+    // Commands to run
+    { command: "node app.js", name: "wordle-bot" },
+  ],
+  {
+    // Concurrently settings
+    prefix: "test {index} - {name}",
+    killOthers: ["failure", "success"],
+    restartTries: 3,
+  }
+).then(
+  function onSuccess(exitInfo) {
+    console.log("\n========\nSuccess\n========\n");
+    // for (exitData in exitInfo) {
+    //     console.log(
+    //         `Index ${exitData}:\n\tCommand: ${exitData}\n\tExit Code: ${exitData}\n`
+    //     );
+    // }
+    process.exit();
+  },
+  function onFailure(exitInfo) {
+    console.log("\n========\nFailure\n========\n");
+    // for (exitData in exitInfo) {
+    //     console.log(
+    //         `Index ${exitData}:\n\tCommand: ${exitData}\n\tExit Code: ${exitData}\n`
+    //     );
+    // }
+    process.exit();
+  }
+);
