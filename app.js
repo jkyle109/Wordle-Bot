c4cdc67afa4a73dba5ff4e71154a7bfde0ae4f24
Jonathan Edey <55252373+jkyle109@users.noreply.github.com>
2022-05-11 05:54:37 -0400

    Fix port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import {
 } from "./commands.js";
 import { handleInteractions } from "./websocket.js";
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 // Create Express App
 const app = express();
